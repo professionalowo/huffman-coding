f2ccd706f0a301fbeaef110701b0c31ff2c7cf4a
professionalowo <martin.brandl1307@gmail.com>
2025-03-21 20:14:08 +0100

    added tree structure

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,2 +1,26 @@
+use node::Node;
+
 mod node;
 mod node_data;
+
+pub(crate) struct HuffmanTree {
+    root: Option<Node>,
+}
+
+impl HuffmanTree {
+    pub fn new(root: Option<Node>) -> Self {
+        Self { root }
+    }
+
+    pub fn new_root(root: Node) -> Self {
+        Self { root: Some(root) }
+    }
+
+    pub fn empty() -> Self {
+        Self { root: None }
+    }
+
+    pub fn root(&self) -> Option<&Node> {
+        self.root.as_ref()
+    }
+}
