7bcbfea6096c88e55e07e1b1e9c547a4664c9927
professionalowo <martin.brandl1307@gmail.com>
2025-03-21 20:37:21 +0100

    impl Ord for Node

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -13,7 +13,27 @@ pub(crate) enum Node {
 impl Node {
     pub fn data(&self) -> &NodeData {
         match self {
-            Self::Leaf(data) | Self::Node { data, .. } => data
+            Self::Leaf(data) | Self::Node { data, .. } => data,
         }
-    } 
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.data().cmp(other.data())
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for Node {}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.data() == other.data()
+    }
 }
