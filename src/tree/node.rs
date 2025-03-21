748e8ad5ad95dc2b6fb3434cac75dc40de201b84
professionalowo <martin.brandl1307@gmail.com>
2025-03-21 20:11:12 +0100

    started implementing huffman coding by implementing a btree

---
+++ src/tree/node.rs
@@ -0,0 +1,18 @@
+use super::node_data::NodeData;
+
+pub(crate) enum Node {
+    Leaf(NodeData),
+    Node {
+        data: NodeData,
+        left: Box<Node>,
+        right: Box<Node>,
+    },
+}
+
+impl Node {
+    pub fn data(&self) -> &NodeData {
+        match self {
+            Self::Leaf(data) | Self::Node { data, .. } => data
+        }
+    } 
+}
---
+++ src/tree/node_data.rs
@@ -0,0 +1,40 @@
+use std::cmp::Ordering;
+
+pub(crate) struct NodeData {
+    data: char,
+    freq: u64,
+}
+
+impl NodeData {
+    pub fn new(data: char, freq: u64) -> Self {
+        Self { data, freq }
+    }
+
+    pub fn data(&self) -> &char {
+        &self.data
+    }
+
+    pub fn freq(&self) -> &u64 {
+        &self.freq
+    }
+}
+
+impl Ord for NodeData {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.freq().cmp(other.freq())
+    }
+}
+
+impl PartialOrd for NodeData {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for NodeData {}
+
+impl PartialEq for NodeData {
+    fn eq(&self, other: &Self) -> bool {
+        self.freq() == other.freq()
+    }
+}
